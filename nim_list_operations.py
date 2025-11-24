9b232982848d464d56c1549d1a7548999a9c686b
Annas Malik <45899829+annasmalikid@users.noreply.github.com>
2025-11-24 17:54:53 +0700

    Create nim_list_operations.py

---
+++ nim_list_operations.py
@@ -0,0 +1,10 @@
+nim_list = [3,1,2,3,1,0,5,2,0]
+
+print("Indeks ke-4:", nim_list[3])
+
+print("Indeks ke-2 sampai terakhir:", nim_list[1:])
+
+print("Indeks terakhir:", nim_list[-1])
+
+nim_list[3] = '0'
+print("Mengubah indeks ke-4 menjadi 0:", nim_list)
